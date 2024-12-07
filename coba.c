241e02e3a171bf1703b12fca33803858abc5d799
thaskia <thaskia67@gmail.com>
2024-12-07 10:50:30 +0700

    first commit

--- coba.c
+++ coba.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main(){
-    printf("Hello World");
+int main(){
+    int i;
+    for(i=1;i<=10;i++){
+        printf("%d",i);
+        printf("\n");
+        
+    }
+    return 0;
 }
---
+++ pertemuan.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+void main()
+{
+    int umur;
+
+    printf("masukkan umur klian :");
+}
+
+
